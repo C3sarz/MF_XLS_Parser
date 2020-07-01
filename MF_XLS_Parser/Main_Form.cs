ffd9daf9c867d0a988c622ca531c0944b7c2b14e
cesarz <cesarz@ksu.edu>
2020-07-01 12:33:01 -0400

    Add project files.

---
+++ MF_XLS_Parser/Main_Form.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace MF_XLS_Parser
+{
+    public partial class Main_Form : Form
+    {
+        public Main_Form()
+        {
+            InitializeComponent();
+        }
+    }
+}
