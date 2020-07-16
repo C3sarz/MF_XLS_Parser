af8958dbc4af713843fd93ab0b724ade3513fe52
cesarz <cesarz@ksu.edu>
2020-07-16 11:14:22 -0400

    pre-branch

--- MF_XLS_Parser/Main_Form.cs
+++ MF_XLS_Parser/Main_Form.cs
@@ -409,17 +409,16 @@ namespace MF_XLS_Parser
                                     output.currentSheet.Cells[newSheetPositionY, newSheetPositionX + 6] = group;
                                     output.currentSheet.Cells[newSheetPositionY, newSheetPositionX + 7] = category;
                                     output.currentSheet.Cells[newSheetPositionY, newSheetPositionX + 8] = subCategory;
+                                    if (!TypeStorage.ContainsKey(name))
+                                    {
+                                        TypeStorage.Add(name, new TypeBlock(section, group, category, subCategory));
+                                    }
                                 }
 
                                 //Date copying
 
                                 output.currentSheet.Cells[newSheetPositionY, newSheetPositionX + 10] = input.Month;
                                 output.currentSheet.Cells[newSheetPositionY, newSheetPositionX + 11] = input.Year;
-
-                                if (!TypeStorage.ContainsKey(name))
-                                {
-                                    TypeStorage.Add(name,new TypeBlock(section,group,category,subCategory));
-                                }
 
                                 newSheetPositionY++;
                             }
